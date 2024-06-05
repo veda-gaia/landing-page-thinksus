350e9a28dfda444a242ad4a697dbc17046158269
RenanLucenaDosSantos <renan@codefabrik.dev>
2024-06-05 10:13:02 -0300

    Post Avaliation results

--- src/app/logged/results/results.component.ts
+++ src/app/logged/results/results.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent {
+  avaliationStatus = ''
+  list: any[] = []
+  filteredList: any[] = []
 
+  constructor() {
+    this.avaliationStatus = 'pre-avaliation'
+
+    this.loadList()
+  }
+
+  loadList() {
+    this.list = [
+      {
+        seal: '',
+        title: 'Exemplo LTDA',
+        buyValue: 1000000,
+        currency: 'BRL',
+        score: 55.4,
+        createdAt: '00/00/0000'
+      },
+      {
+        seal: '',
+        title: 'Exemplo LTDA',
+        buyValue: 1000000,
+        currency: 'BRL',
+        score: 55.4,
+        createdAt: '00/00/0000'
+      },
+      {
+        seal: '',
+        title: 'Exemplo LTDA',
+        buyValue: 1000000,
+        currency: 'BRL',
+        score: 55.4,
+        createdAt: '00/00/0000'
+      },
+      {
+        seal: '',
+        title: 'Exemplo LTDA',
+        buyValue: 1000000,
+        currency: 'BRL',
+        score: 55.4,
+        createdAt: '00/00/0000'
+      },
+      {
+        seal: '',
+        title: 'Exemplo LTDA',
+        buyValue: 1000000,
+        currency: 'BRL',
+        score: 55.4,
+        createdAt: '00/00/0000'
+      },
+    ]
+    this.filteredList = this.list
+  }
+
+  toggleAvaliationStatus() {
+    if(this.avaliationStatus === 'pre-avaliation') {
+      this.avaliationStatus = 'post-avaliation'
+    } else this.avaliationStatus = 'pre-avaliation'
+  }
 }

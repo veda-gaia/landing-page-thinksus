889a275ae4edf0f6b6034e9e34b43cda232056f4
RenanLucenaDosSantos <renan@codefabrik.dev>
2024-06-06 11:30:49 -0300

    .

--- src/app/logged/improvements/improvements.component.ts
+++ src/app/logged/improvements/improvements.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./improvements.component.scss']
 })
 export class ImprovementsComponent {
+  infoArray: any[] = []
+  openAccordions: number[] = []
 
+  constructor(){
+    this.infoArray = [
+      {
+        number: 1,
+        title: 'Recursos naturais',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
+      },
+      {
+        number: 2,
+        title: 'Natureza',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
+      },
+      {
+        number: 3,
+        title: 'Clima e risco',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
+      },
+      {
+        number: 4,
+        title: 'Gestão de resíduos',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
+      },
+    ]
+  }
+
+  toggleAccordion(number: number) {
+    // Remove se tiver
+    if(this.openAccordions.includes(number)) {
+      this.openAccordions = this.openAccordions.filter((i) => {
+        return i !== number
+      })
+      return
+    }
+    
+    // Inclui se nao tiver
+    this.openAccordions.push(number)
+  }
 }

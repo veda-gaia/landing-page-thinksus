280d1f4f58694f7dd7a3a7b981d344c9d5fde3fc
RenanLucenaDosSantos <renan@codefabrik.dev>
2024-06-25 20:29:57 -0300

    THIN-71

--- src/app/logged/improvements/improvements.component.ts
+++ src/app/logged/improvements/improvements.component.ts
@@ -6,32 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./improvements.component.scss']
 })
 export class ImprovementsComponent {
-  infoArray: any[] = []
   openAccordions: number[] = []
+  selectedESG = ''
 
   constructor(){
-    this.infoArray = [
-      {
-        number: 1,
-        title: 'Recursos naturais',
-        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
-      },
-      {
-        number: 2,
-        title: 'Natureza',
-        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
-      },
-      {
-        number: 3,
-        title: 'Clima e risco',
-        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
-      },
-      {
-        number: 4,
-        title: 'Gestão de resíduos',
-        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\','
-      },
-    ]
+
+  }
+
+  changeSelectedESG(number: number) {
+    this.openAccordions = []
+
+    if(number <= 4) this.selectedESG = "environmental"
+    if(number > 4 && number <= 8) this.selectedESG = "social"
+    if(number > 8 && number <= 12) this.selectedESG = "governmental"
   }
 
   toggleAccordion(number: number) {

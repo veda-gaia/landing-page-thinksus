21d2f1c986ddefe72455c5f4cb9eb6bf53246975
iagofeelix <93677121+iagofeelix@users.noreply.github.com>
2024-07-26 11:18:18 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
 
     api: {
-        path: 'https://api-prod.thinksus.com',
+        path: 'http://ec2-54-200-130-21.us-west-2.compute.amazonaws.com:3001',
     },
  
     encrypt_key: '5e99d833-bcfa-411e-ad92-97686ca6890c',

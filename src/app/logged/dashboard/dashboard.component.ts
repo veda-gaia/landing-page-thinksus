32e511379e9162e3750d90977ed04d2f82009c41
RenanLucenaDosSantos <renan@codefabrik.dev>
2024-08-22 16:46:17 -0300

    THIN-132

--- src/app/logged/dashboard/dashboard.component.ts
+++ src/app/logged/dashboard/dashboard.component.ts
@@ -41,6 +41,7 @@ export class DashboardComponent {
   };
 
   odsScoreArray: any[] = [];
+  postOdsScoreArray: any[] = [];
   allCompleteAvaliations: any[] = []
   postAvaliationInfo: any;
 
@@ -184,6 +185,7 @@ export class DashboardComponent {
         if (postAvaliation) {
           this.postAvaliationInfo = postAvaliation;
           this.avaliationStatus = 'post-avaliation';
+          this.postOdsScoreArray = postAvaliation.odsScore
           console.log(postAvaliation);
         }
         

09c7615a895a1e327783cffd994e38bde7905cff
bruno-bogochvol <brunobogochvol@gmail.com>
2025-11-11 12:09:39 -0300

    import ptBr

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 /// <reference types="@angular/localize" />
 
+import { registerLocaleData } from '@angular/common';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import ptBr from '@angular/common/locales/pt';
 
 import { AppModule } from './app/app.module';
 
+registerLocaleData(ptBr, 'pt-BR');
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
